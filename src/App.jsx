57dce852a1d6ed3259614128567fffb4edb2e5ca
Hemant Prajapati <hemantpraja88@gmail.com>
2024-05-24 00:19:00 +0530

    configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authSerrvice from './appwrite/auth.js'
+import { login, logout } from './redux/authSlice.js'
+import { Header } from './components/index.js'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>asbfas</h1>
-    </>
-  )
+  useEffect(() => {
+    authSerrvice.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setloading(false))
+  }), []
+  return loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-grey-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <Outlet/>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
